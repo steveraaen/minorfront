57d98190c3e9bee6cef06df8cb266f4dc45e9b5c
Steveraaen <sraaen@gmail.com>
2019-02-16 16:54:31 -0500

    recharts

--- src/components/MainChart.js
+++ src/components/MainChart.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip } from 'recharts';
+import { AreaChart, BarChart, Legend, Bar, Area, XAxis, YAxis, CartesianGrid, ResponsiveContainer, Tooltip } from 'recharts';
 
 
 function MainChart(props) {
@@ -7,21 +7,24 @@ function MainChart(props) {
 
 
     return (
-      <AreaChart
-        width={500}
-        height={400}
+    	<div>
+     <BarChart
+        width={2000}
+        height={600}
         data={props.bestMinors}
         margin={{
-          top: 10, right: 30, left: 0, bottom: 0,
+          top: 5, right: 30, left: 20, bottom: 5,
         }}
       >
         <CartesianGrid strokeDasharray="3 3" />
         <XAxis dataKey="team" />
         <YAxis />
         <Tooltip />
-        <Area type="monotone" dataKey="playerCount" stackId="1" stroke="#8884d8" fill="#8884d8" />
-        <Area type="monotone" dataKey="yr"  stackId="2" stroke="#8884d8" fill="#8884d8" />
-      </AreaChart>
+        <Legend />
+        <Bar dataKey="playerCount" fill="#8884d8" />
+        
+      </BarChart>
+      </div>
     );
   }
 
